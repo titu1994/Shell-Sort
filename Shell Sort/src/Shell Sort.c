1cd09f1e6d586d7cef1c7fb811a4a7a7c66b5d14
titu1994 <titu1994@gmail.com>
2013-10-05 18:18:30 +0530

    Finished

--- Shell Sort/src/Shell Sort.c
+++ Shell Sort/src/Shell Sort.c
@@ -13,7 +13,54 @@
 
 int main(void) {
 
-	printf("Enter 2 elements\n");
+	int *a = NULL, i;
+	int n;
+
+	void shellSort(int *a, int size);
+
+	setbuf(stdout, NULL);
+
+	printf("Enter number of elements : \n");
+	scanf("%d",&n);
+
+	a = (int*) calloc(n, sizeof(int));
+
+	printf("Enter the elements\n");
+
+	for(i = 0; i < n; i++){
+		scanf("%d",&a[i]);
+	}
+
+	shellSort(a, n);
+
+	for(i = 0; i < n; i ++){
+		printf("%d\t",a[i]);
+	}
 
 	return EXIT_SUCCESS;
+}
+
+void shellSort(int arr[], int size){
+	int gap = 5, i, j;
+	int temp;
+
+	while(gap >= 1){
+
+		for(i = gap; i < size; i++){
+
+			temp = arr[i];
+
+			for(j = i; j >= gap && arr[j - gap] > temp; j -= gap){
+
+				arr[j] = arr[j - gap];
+
+			}
+
+			arr[j] = temp;
+		}
+
+		gap -= 2;
+
+	}
+
 }
